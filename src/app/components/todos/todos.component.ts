e6c936f01f3697cab7bddb5c6dab76119484c39d
Katherine Lewis <kale8432@colorado.edu>
2019-09-24 15:49:32 -0600

    Render todos as list items

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -17,6 +17,16 @@ export class TodosComponent implements OnInit {
         id: 1,
         title: "Todo One",
         completed: false,
+      },
+      {
+        id: 2,
+        title: "Todo Two",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Todo Three",
+        completed: true,
       }
     ]
   }
